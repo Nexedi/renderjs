83d9304cbafebaac02c7b68359a76f10b9ddd5f2
Ivan Tyagov <ivan@nexedi.com>
2012-11-14 14:24:39 +0200

    Add example how a gadget may require its own javascript files to be loaded using requirejs.

---
+++ examples/requirejs_gadget_dependency_load/require-gadget-implementation.js
@@ -0,0 +1,5 @@
+// This module contains gagdets JavaScript implementation code
+
+function setLoaded() {
+  $("#loading").text('Loaded now using requirejs.');
+}
---
+++ examples/requirejs_gadget_dependency_load/require-gadget.js
@@ -0,0 +1,7 @@
+// This file defines all required javascript files for this gadget
+
+require(["require-gadget-implementation.js"],
+         function (domReady) {
+           // required by this gadget code is loaded so we can use it
+           setLoaded();
+         });
