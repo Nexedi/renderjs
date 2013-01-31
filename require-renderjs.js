daf7d792580b6ce5eac1bc8f38a1a8208a4b95a7
Ivan Tyagov <ivan@nexedi.com>
2013-01-31 16:35:13 +0200

    Set proper dependencies otherwise requirejs will load randomly causing unexpected failures.

--- require-renderjs.js
+++ require-renderjs.js
@@ -3,14 +3,16 @@ require.config({
   paths: {
     jquery: "lib/jquery/jquery",
     "jquery.json": "lib/json/jquery.json.min",
-    "davstorage": "lib/jio/davstorage",
-    "md5": "lib/jio/md5",
-    "jio": "lib/jio/jio",
+    davstorage: "lib/jio/davstorage",
+    md5: "lib/jio/md5",
+    jio: "lib/jio/jio",
     renderjs: "renderjs"
   },
   shim: {
     "jquery.json": [ "jquery" ],
-    renderjs: [ "jquery", "jquery.json", "md5", "jio", "davstorage"]
+    jio: ["md5"],
+    davstorage: ["jio"],
+    renderjs: [ "jquery", "jquery.json", "jio", "md5", "davstorage" ]
   }
 });
 
