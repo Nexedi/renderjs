4a2742adcd23a68e503e8acc55082d3ca52b761b
Sven Franck <sven.franck@nexedi.com>
2013-02-08 10:07:50 +0100

    jqm+renderjs - added some examples

---
+++ examples/jqm_1/controller.js
@@ -0,0 +1,34 @@
+/*jslint browser: true, indent : 2, maxerr : 50, nomen : true, regexp : true,
+sloppy : true */
+/*global define: false, $: false, document: false, require: false */
+
+define([], function () {
+  "use strict";
+
+  var start = function () {
+    require(['overrides', 'jquery', 'jqm', 'jquery.json', 'renderjs'],
+      function () {
+
+        // this is my application controller
+        // as I have not used renderJS before, this is where all my
+        // client-side "stuff" happens (ajax form requests, content modifications,
+        // on demand loading of additional plugins
+
+        // START
+        if ($.mobile.autoInitializePage === false) {
+          // initialize JQM
+          $.mobile.initializePage();
+
+          // initialize renderJS
+          RenderJs.init();
+ 
+          // when all gadgets are loaded make sure JQM renders them
+          RenderJs.bindReady(
+            function () {
+              $("[data-gadget]").trigger('create');
+            });
+        }
+      });
+  };
+  return {"start": start};
+});
---
+++ examples/jqm_2/controller.js
@@ -0,0 +1,48 @@
+/*jslint browser: true, indent : 2, maxerr : 50, nomen : true, regexp : true,
+sloppy : true */
+/*global define: false, $: false, document: false, require: false */
+
+define([], function () {
+  "use strict";
+
+  var start = function () {
+    require(['overrides', 'jquery', 'jqm', 'jquery.json', 'renderjs'],
+      function () {
+
+        // this is my application controller
+        // as I have not used renderJS before, this is where all my
+        // client-side "stuff" happens (ajax form requests, content modifications,
+        // on demand loading of additional plugins
+
+        // detect gadgets on a page
+        $(document).on('pagebeforeshow', 'div.ui-page', function () {
+          $('[data-gadget]').filter(
+            function () { 
+              return $(this).jqmData("bound") !== true;
+            }
+          )
+          .each(
+            function () {
+              // trigger gadget loading
+            }
+          );
+        });
+
+        // START
+        if ($.mobile.autoInitializePage === false) {
+          // initialize JQM
+          $.mobile.initializePage();
+
+          // initialize renderJS
+          RenderJs.init();
+ 
+          // when all gadgets are loaded make sure JQM renders them
+          RenderJs.bindReady(
+            function () {
+              $("[data-gadget]").trigger('create');
+            });
+        }
+      });
+  };
+  return {"start": start};
+});
