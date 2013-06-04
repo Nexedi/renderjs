43d97b8d03253817c188f6ca788764f051c9b77b
Romain Courteaud <romain@nexedi.com>
2013-06-04 11:09:00 +0200

    Global gadget to display browser and preview
    No interaction for now.

---
+++ gadget/filebrowser_and_preview.js
@@ -0,0 +1,53 @@
+/*global document, jQuery */
+// filebrowser.html?file=browser%3A%2F%2Fbrowse%2Fls%2F
+"use strict";
+(function (document, $) {
+
+  var getParameter = function(searchString, paramName) {
+    var i, val, params = searchString.split("&");
+
+    for (i=0;i<params.length;i++) {
+      val = params[i].split("=");
+      if (val[0] == paramName) {
+        return decodeURIComponent(val[1]);
+      }
+    }
+    return null;
+  };
+
+  var mapUrl = function (url) {
+    var searchString = url.href.split("?")[1],
+      fileToDisplay;
+
+    if (searchString) {
+      fileToDisplay = getParameter(searchString, "file");
+
+      if (fileToDisplay) {
+
+        $("body").html(
+          '<iframe src="' +
+          // XXX Hardcoded gadget to load
+          'filebrowser.html?file=' + fileToDisplay +
+          '">' +
+          '<p>Your browser does not support iframes.</p>' +
+          '</iframe">');
+
+        $("body").append(
+          '<iframe src="' +
+          // XXX Hardcoded gadget to load
+          'preview.html' +
+          '">' +
+          '<p>Your browser does not support iframes.</p>' +
+          '</iframe">');
+
+      } else {
+        $(this).text("No parameter found in url");
+      }
+    }
+  };
+
+  $(document).ready(function () {
+    mapUrl(window.location);
+  });
+
+}(document, jQuery));
