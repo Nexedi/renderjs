9df396f9314cb6415169917637d5dc993e1e53f2
Xiaowu Zhang <xiaowu.zhang@tiolive.com>
2016-11-21 12:37:10 +0100

    gadget iniitialization should not be cancellable

---
+++ test/cancel_gadget.js
@@ -0,0 +1,12 @@
+/*jslint nomen: true*/
+(function (window, rJS) {
+  "use strict";
+
+  var gk = rJS(window);
+  gk.
+    ready(function (g) {
+    })
+    .declareMethod('render', function () {
+      return "";
+    });
+}(window, rJS));
